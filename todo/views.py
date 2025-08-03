24244a0d6a32220badc8f14517007bb8f78176db
Kumarkhatri2003 <kumarkhatri836@gmail.com>
2025-08-03 19:14:26 +0545

    first commit for TODO list in Django

---
+++ todo/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Task #importing the models Taks
+from .forms import TaskForm
+
+def task_list(request):
+    tasks = Task.objects.all() #get all tasks from the database
+    form = TaskForm()
+    
+    if request.method =='POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list') 
+    
+    return render(request, 'todo/task_list.html',{
+        'tasks':tasks,
+        'form' : form
+        })
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task,id=task_id) 
+    task.completed = True
+    task.save()
+    return redirect('task_list')
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('task_list')
